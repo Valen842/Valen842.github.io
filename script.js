f4406bf2366f9dc54d96d54c0d58b7f6834e7913
Valentino <vbisbano@hotmail.com>
2024-01-15 10:09:20 -0300

    hola

---
+++ script.js
@@ -0,0 +1,22 @@
+"use strict";
+
+window.addEventListener("load", () => {
+  var etrr = document.querySelector(".img-etrr");
+  var fce = document.querySelector(".img-fce");
+  var gottert = document.querySelector("img-gottert");
+
+
+  etrr.addEventListener("click", function () {
+    window.location.href = "http://www.tecnicarobertorocca.edu.ar/campana";
+  });
+
+  fce.addEventListener("click", function () {
+    window.location.href = "https://www.cambridgeenglish.org/exams-and-tests/first/";
+  });
+
+  gottert.addEventListener("click", function () {
+    window.location.href = "https://www.gottert.com.ar/";
+  });
+
+  console.log("hola");
+});
