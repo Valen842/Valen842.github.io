3d3b414a2cda62db52182d0276e06cf3cb6d70c0
Valentino <vbisbano@hotmail.com>
2024-01-16 08:08:04 -0300

    Social media links

--- script.js
+++ script.js
@@ -4,9 +4,12 @@ window.addEventListener("load", () => {
   var etrr = document.querySelector(".img-etrr");
   var fce = document.querySelector(".img-fce");
   var gottert = document.querySelector("img-gottert");
+  var github = document.querySelector('img-git');
+  var linkedin = document.querySelector('img-in');
+  var twitter = document.querySelector('img-x');
 
 
-  etrr.addEventListener("click", function () {
+  etrraddEventListener("click", function () {
     window.location.href = "http://www.tecnicarobertorocca.edu.ar/campana";
   });
 
@@ -17,5 +20,18 @@ window.addEventListener("load", () => {
   gottert.addEventListener("click", function () {
     window.location.href = "https://www.gottert.com.ar/";
   });
+
+  github.addEventListener("click", function () {
+    window.location.href = "https://github.com/Valen842";
+  });
+
+  linkedin.addEventListener('click', function(){
+    window.location.href = 'https://www.linkedin.com/in/valentino-bisbano-068413259/';
+  });
+
+  twitter.addEventListener('click', function(){
+    window.location.href = 'https://twitter.com/Valenbisbano';
+  });
+
 
 });
